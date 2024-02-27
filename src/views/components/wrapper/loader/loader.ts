759075db317e1288d145ac8c45452beb4ed8f80e
David Michel <david.michel2809@gmail.com>
2024-02-27 16:39:02 +0100

    refacto: rename info file to informations

--- src/views/components/wrapper/loader/loader.ts
+++ src/views/components/wrapper/loader/loader.ts
@@ -1,6 +1,6 @@
 import styles from "./loader.module.css"
 import { spinner } from "./spinner/spinner"
-import { informationsElement } from "./informations/info";
+import { informationsElement } from "./informations/informations";
 
 export const loader = document.createElement("div");
 loader.classList.add(styles.loader)
