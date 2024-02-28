b445cf3c08b2eefaf4d3220a0450a364c4a54237
David Michel <david.michel2809@gmail.com>
2024-02-28 16:10:08 +0100

    style: style adjustment, add external logo for unavailable links

--- src/views/components/wrapper/unavailableLink/unavailableLink.ts
+++ src/views/components/wrapper/unavailableLink/unavailableLink.ts
@@ -2,9 +2,13 @@ import styles from "./unavailableLink.module.css"
 import alldebridFavicon from "./alldebrid_favicon.png"
 
 export const unavailableLinkElement = document.createElement("a")
-unavailableLinkElement.textContent = "En cours de traitement"
+unavailableLinkElement.textContent = "Magnet en traitement"
 unavailableLinkElement.classList.add("butt", styles.unavailableLink)
 
 const alldebridLogo = document.createElement("img")
 alldebridLogo.src = alldebridFavicon
-unavailableLinkElement.append(alldebridLogo)
+
+const externalLinkIcon = document.createElement("span")
+externalLinkIcon.classList.add("ico_external-link")
+
+unavailableLinkElement.append(alldebridLogo, externalLinkIcon)
